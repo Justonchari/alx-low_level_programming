aa204f86eea93b5374fb347dc48c009d90a280ec
Justonchari <justinonchari@gmail.com>
2023-03-10 16:58:35 -0800

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - this is the main function
+ * @argc: arguent count
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+        printf("%d\n", argc - 1);
+        return (0);
+}
